de75f2f5d7f40b63531058f89435a578eff3d6cc
haskizme <haskellb2017@gmail.com>
2024-11-12 16:22:47 -0700

    Added firebase to project. Firebase and cloud firestore was installed but not code to actually write to database yet

--- lib/main.dart
+++ lib/main.dart
@@ -6,11 +6,16 @@ import 'package:golf_accelerator_app/screens/login/login.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'Observer/provider_observer.dart'; // Import flutter_riverpod for ProviderScope
 
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(
       ProviderScope(
         observers: [
